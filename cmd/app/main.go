166a367d57ae63c446c8fb2d2eec89bf77d03028
kharitmaboy <ffgost@mail.ru>
2023-12-14 03:16:26 +0300

    Added server, routes, empty handlers and some models

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	todo "github.com/kharitmaboy/simple-todo-list"
+	"github.com/kharitmaboy/simple-todo-list/pkg/handler"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello")
+	handlers := new(handler.Handler)
+
+	srv := new(todo.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error while running http server: %s", err.Error())
+	}
 }
